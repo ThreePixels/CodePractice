6ce91b0864305a520ebd401214400c7839258601
changhong <[liuchanghong@outlook.com]>
2017-12-20 16:34:03 +0800

    init project transition

---
+++ Transition/Transition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Transition
+//
+//  Created by 刘长虹 on 2017/12/20.
+//  Copyright © 2017年 刘长虹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Transition/Transition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Transition
+//
+//  Created by 刘长虹 on 2017/12/20.
+//  Copyright © 2017年 刘长虹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
